39e3870985580a16f1f7a53d882bcf6deba0c18c
willowcheng <e.wingus@Gmail.com>
2015-03-12 14:22:01 -0400

    Ruby numbers test

--- hello.rb
+++ hello.rb
@@ -1,3 +1,10 @@
-print 'Please enter your name: '
-name = gets
-puts "Hello #{name}!"
+name = 'Willow'
+string = "My name is #{name}"
+
+puts string
+
+year = 2015
+puts "The year is #{year}"
+
+future = 5
+puts "In #{future} years, the year will be #{year + future}"
