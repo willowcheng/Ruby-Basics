2e90f2c345a34a16e40d04225c7e5bb519c74979
willowcheng <e.wingus@Gmail.com>
2015-03-12 15:05:39 -0400

    Ruby methods test

---
+++ methods.rb
@@ -0,0 +1,11 @@
+# Print out that we're adding two numbers together.
+# And them return the sum of the two numbers.
+def add(a, b)
+  puts "Adding #{a} and #{b}:"
+  return a + b
+end
+
+
+puts add(2, 3)
+add(5, 5)
+add(6, 6)
